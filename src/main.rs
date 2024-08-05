329235b55e08edb3231f67a08e58e2998a6d89b6
Malte PC <malteaschenbach@gmail.com>
2024-08-06 00:39:54 +0200

    some beauty

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use core::f64;
 use std::{f64::consts::PI, time::Duration};
 
+use num_format::{Locale, ToFormattedString};
 use simple_logger::SimpleLogger;
+
+const LOCALE: Locale = Locale::de;
 
 fn main() {
     SimpleLogger::new().init().unwrap();
 
     log::info!("Computing pi with the Georgy Leibniz Series...");
-    time(georgy_leibniz_series, u64::MIN, 1000000000);
+    time(georgy_leibniz_series, 100000000, 200000000);
 
     log::info!("Computing pi with the Nilakantha Series...");
-    time(nilakantha_series, u64::MIN, 1000000000);
+    time(nilakantha_series, 100000000, 200000000);
 }
 
 /// Nilakantha Series
-/// /// $$\pi=3+\frac{4}{2*3*4}-\frac{4}{4*5*6}+\frac{4}{5*6*7}+\ldots$$
+///
+/// $$\pi=3+\frac{4}{2*3*4}-\frac{4}{4*5*6}+\frac{4}{5*6*7}-\ldots$$
+///
+/// It took 997.002421ms (156 257 628 iterations) to compute 15 digits correct.
 fn nilakantha_series(n: u64) -> f64 {
     let mut pi = 3.0;
     for i in 0..n {
@@ -26,7 +32,10 @@ fn nilakantha_series(n: u64) -> f64 {
     pi
 }
 /// Georgy Leibniz series
+///
 /// $$\frac{\pi}{4}=1-\frac{1}{3}+\frac{1}{5}-\frac{1}{7}+\ldots$$
+///
+/// It took 998.99085ms (164 658 389 iterations) to compute 10 digits correct.
 fn georgy_leibniz_series(n: u64) -> f64 {
     let mut pi_fourths = 0.0;
     for i in 0..n {
@@ -52,7 +61,7 @@ fn time(approximator: impl Fn(u64) -> f64, i_min: u64, i_max: u64) {
 
         if dt < Duration::from_secs(1) {
             lower_bound = i;
-            if lower_bound == upper_bound - 1 {
+            if lower_bound >= upper_bound - 1000 {
                 break;
             }
         } else {
@@ -63,10 +72,14 @@ fn time(approximator: impl Fn(u64) -> f64, i_min: u64, i_max: u64) {
     let correct_n = accurate_to_digit(PI, calculated);
 
     log::info!(
-        "It took {:?} ({} iterations) to compute {} digits correct.",
+        "It took {:?} ({} iterations) to compute {} digits correct with a delta of {} - {} = {} ({} %)",
         dt,
-        lower_bound,
-        correct_n
+        lower_bound.to_formatted_string(&LOCALE),
+        correct_n.to_formatted_string(&LOCALE),
+        PI,
+        calculated,
+        PI - calculated,
+        (PI - calculated) / PI * 100.0
     );
 }
 
